f2f8af3f2d031bd973821de7234b66a6a16e248c
Mar Cejas <mvcejas@gmail.com>
2012-08-11 21:54:05 +0800

    update changes

--- address.php
+++ address.php
@@ -22,87 +22,84 @@
 <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap.min.css"/>
 <link rel="stylesheet" href="/css/custom.css"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="/js/bootstrap-typeahead.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?sensor=true"></script>
+<script src="/js/markerwithlabel.js"></script>
+<script src="/js/map.js"></script>
 <script>
-var lat,lng;
-var address;
-var bounds;
-var infowindow;
-var geo;
-var map;
-var markers=[];
-function init(){
-	geo		= new google.maps.Geocoder();
-	bounds 	= new google.maps.LatLngBounds();
-	var myOptions = {
-		zoom: 9,
-		center: new google.maps.LatLng(14.597466, 121.0092),
-		mapTypeId: google.maps.MapTypeId.HYBRID,
-		panControl: false,
-		streetViewControl: false,		
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.TOP_RIGHT,
-			mapTypeIds: [google.maps.MapTypeId.HYBRID,google.maps.MapTypeId.ROADMAP],
+$(document).ready(function(){
+	$('#country').typeahead({
+		source : function(typeahead,query){
+			return $.get('countries.php',function(data){
+				console.log(data);
+				return typeahead.process(data);
+			});
 		},
-		zoomControlOptions: {
-			position: google.maps.ControlPosition.RIGHT_TOP,
+		property: 'long_name',
+		onselect: function(obj){
+			$('#country_sn').val(obj.short_name);
+			console.log(obj);
 		},
-	};	
-	map 	= new google.maps.Map(document.getElementById('map_canvas'),myOptions);		
-	marker 	= new google.maps.Marker({animation: google.maps.Animation.BOUNCE,draggable:true,});    
-	window.setTimeout(getUsersLocation, 2000);
-	google.maps.event.addListener(map, 'click', function(event) {
-		marker.setPosition(event.latLng);
-		map.panTo(event.latLng);
-		console.log(event.latLng.Xa+'\t'+event.latLng.Ya);
-		console.log(map.getZoom());
 	});
-}
-function getUsersLocation(){
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(function(position) {
-			var pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-			map.panTo(pos);
-			marker.setPosition(pos);
-			marker.setMap(map);			
-		},function(){		
-			marker.setPosition(new google.maps.LatLng(14.597466, 121.0092));
-			marker.setMap(map);
-		});
-	}else{
-		marker.setPosition(new google.maps.LatLng(14.597466, 121.0092));
-		marker.setMap(map);
-	}
-}
-function showMap(){
-	var addr = [];
-	$('.map_addr').each(function(a,b){
-		addr.push(b.value);
-	});
-	address = addr.join(' ');
-	geo.geocode({'address': address},function(results,status){	
-		if(status == google.maps.GeocoderStatus.OK){			
-			map.setCenter(results[0].geometry.location);
-			marker.setPosition(results[0].geometry.location);
-			console.log(results[0].geometry.location.Xa+'\t'+results[0].geometry.location.Ya);
-		}else{
-			alert("Geocode was not successful for the following reason: " + status + ' addr: '+ addr);
-		}
-	});
-}
-google.maps.event.addDomListener(window, 'load', init);
+});
 </script>
 </head>
 <body>
 <?php include 'header.inc'?>
 <div id="map_canvas"></div>
-
+<div class="well" style="width:380px;position:absolute;top:50px;z-index:4;">
+	<form class="form-horizontal" method="post" action="">	
+		<input type="text" id="country_sn" name="country_sn" value=""/>
+		<input type="text" id="lat" name="lat" value=""/>
+		<input type="text" id="lng" name="lng" value=""/>
+		
+		<fieldset>
+			<legend>Address Information</legend>
+			<div class="control-group">
+				<label class="control-label">Address 1</label>
+				<div class="controls">
+					<input type="text" class="input input-large map_addr" name="address"/>
+				</div>
+			</div>
+			<div class="control-group">
+				<label class="control-label">Address 2</label>
+				<div class="controls">
+					<input type="text" class="input input-large map_addr" name="address"/>
+				</div>
+			</div>			
+			<div class="control-group">
+				<label class="control-label">City / Town</label>
+				<div class="controls">
+					<input type="text" class="input input-large map_addr" name="address"/>
+				</div>
+			</div>
+			
+			<div class="control-group">
+				<label class="control-label">State / Province</label>
+				<div class="controls">
+					<input type="text" class="input input-large map_addr" name="address"/>
+				</div>
+			</div>
+			
+			<div class="control-group">
+				<label class="control-label">Zip / Postal</label>
+				<div class="controls">
+					<input type="text" class="input input-large map_addr" name="address"/>
+				</div>
+			</div>
+			
+			<div class="control-group">
+				<label class="control-label">Country</label>
+				<div class="controls">
+					<input type="text" id="country" class="map_addr" name="country" value="" autocomplete="off"/>
+				</div>
+			</div>	
+			
+			<div class="form-actions">
+				<button type="button" class="btn btn-info" onclick="javascript:showMap();"><i class="icon-white icon-map-marker"></i> Show Map</button>&nbsp; <button type="submit" class="btn btn-info">Continue <i class="icon-white icon-chevron-right"></i></button>
+			</div>
+		</fieldset>
+	</form>
+</div>
 </body>
 </html>
-
-<?php
-	$ipa = '166.221.240.10';//$_SERVER['REMOTE_ADDR'];
-	$api = "http://api.ipinfodb.com/v3/ip-city/?key=bc866c34b4048322ad585dc8fb8d2c08f5b4a9c24e3e3e91592948654ef7fa3b&ip=".$ipa."&format=json";
-	$api = file_get_contents($api);
-	$api = json_decode($api);	
-?>
