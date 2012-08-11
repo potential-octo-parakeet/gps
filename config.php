c6c5d229342cfae90c75b6eff31d0bbb8e5fdf24
Mar Cejas <mvcejas@gmail.com>
2012-08-11 19:54:17 +0800

    added config file

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+	define('db_host','localhost');
+	define('db_user','root');
+	define('db_pass','');
+	define('db_name','gps');
+	// init mysqli connection
+	$sql = mysqli(db_name,db_user,db_pass,db_name);
+?>
