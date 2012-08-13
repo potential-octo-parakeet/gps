66cc31e3525dbcf507a5f712e05fed604141b88e
Mar Cejas <mvcejas@gmail.com>
2012-08-13 12:29:20 +0800

    added login

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+	// if session already started, then go to home
+	if(isset($_SESSION['ID']))
+	header("location:home.php");
+    
+	// require config
+	require 'facebook_config.php';
+    require 'config.php';      
+   
+	$loginUrl = $facebook->getLoginUrl(array('scope' => 'publish_actions,email','redirect_uri'=>'http://'.$_SERVER['HTTP_HOST'].'/home.php'));
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta charset="utf-8" />
+<meta name="description" content="Login to GPS Tracker Online."/>
+<title>Account Login</title>
+<link rel="stylesheet" href="http://www.1bohol.com/css/bootstrap.min.css"/>
+</head>
+<body>
+<?php include 'header.inc' ?>
+<div style="width:300px;margin:80px auto;">
+	<a href="<?php echo $loginUrl?>"><img src="http://i.imgur.com/qmxaw.png" alt="Login with Facebook" />
+</div>
+</body>
+</html>
